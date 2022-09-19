4530b53994a267dd92824734d639c1838d9b742a
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-19 19:14:38 +0900

    :white_check_mark: BookRepository 책 조회 관련 테스트 작성
    #description
    1. Book Entity 전체 조회 테스트 완료
    2. Book Entity 단건 조회 테스트 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,9 +1,12 @@
 package site.metacoding.junitproject.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,6 +15,19 @@ public class  BookRepositoryTest {
 
     @Autowired
     private BookRepository repository;
+
+    // @BeforeEach 는 다음 테스트 메서드 까지만 하나의 트랜잭션으로 본다. (전체 X)
+    @BeforeEach
+    public void initGivenData() {
+        String title = "새로운 책의 Title";
+        String author = "새로운 책의 Author";
+
+        Book newBook = Book.builder()
+            .title(title)
+            .author(author)
+            .build();
+        repository.save(newBook);
+    }
 
     // 1. 책 등록 테스트
     @Test
@@ -33,8 +49,36 @@ public class  BookRepositoryTest {
     }
 
     // 2. 책 조회 테스트
+    @Test
+    @DisplayName("책 목록을 조회합니다.")
+    public void findAllTest() {
+        String title = "새로운 책의 Title";
+        String author = "새로운 책의 Author";
+
+        // when
+        List<Book> findBooks = repository.findAll();
+
+        // then
+        assertThat(findBooks.get(0).getTitle()).isEqualTo(title);
+        assertThat(findBooks.get(0).getAuthor()).isEqualTo(author);
+
+    }
 
     // 2-1. 책 단건 조회 테스트
+    @Test
+    @DisplayName("책 목록 중 한 건을 조회합니다.")
+    public void findDetailTest() {
+        String title = "새로운 책의 Title";
+        String author = "새로운 책의 Author";
+
+        // when
+        Book findBook = repository.findById(1L).get();
+
+        // then
+        assertThat(findBook.getTitle()).isEqualTo(title);
+        assertThat(findBook.getAuthor()).isEqualTo(author);
+
+    }
 
     // 3. 책 수정 테스트
 
