73c95b1e315d3112dc4191bc27a4aa4650dfd746
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-19 18:10:32 +0900

    :white_check_mark: BookRepository 책 등록 테스트
    #description
    1. Book Entity 등록 테스트 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 로딩한다. (단위 테스트)
 public class  BookRepositoryTest {
 
@@ -15,7 +17,19 @@ public class  BookRepositoryTest {
     @Test
     @DisplayName("책을 하나 등록합니다.")
     public void saveTest() {
-        System.out.println("책 등록 테스트 실행");
+
+        // given (데이터 준비)
+        Book newBook = Book.builder()
+            .title("새로운 책의 Title")
+            .author("새로운 책의 author")
+            .build();
+
+        // when (테스트 실행)
+        Book saveBook = repository.save(newBook);
+
+        // then (검증, 실제 값 / 기대 값)
+        assertThat(saveBook.getTitle()).isEqualTo(newBook.getTitle());
+        assertThat(saveBook.getAuthor()).isEqualTo(newBook.getAuthor());
     }
 
     // 2. 책 조회 테스트
