26f42f8e7a274ad5eed24f9a55ccc0be107c343d
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-21 20:30:59 +0900

    :sparkles: 서비스 레이어 테스트 (실제 DB)
    #description
    1. 진짜 DB 를 사용한 서비스 레이어 책 등록 테스트 로직 작성완료
    2. 하지만 이미 레포지토리 테스트를 마친 상태기 때문에 굳이 이렇게 할 필요는 없다.

--- src/main/java/site/metacoding/junitproject/web/dto/BookSaveRequestDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookSaveRequestDto.java
@@ -1,13 +1,22 @@
 package site.metacoding.junitproject.web.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import site.metacoding.junitproject.domain.Book;
 
-// 기본 생성자로 역직렬화를 수행, 현재 아무런 생성자 없으므로 @NoArgConstructor 불필요
+// 기본 생성자로 역직렬화를 수행, 현재 생성자가 있으므로 @NoArgConstructor 필요
+@NoArgsConstructor
 @Getter
 public class BookSaveRequestDto {
     private String title;
     private String author;
+
+    @Builder
+    public BookSaveRequestDto(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
 
     public Book toEntity() {
         return Book.builder()
--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,4 +1,49 @@
 package site.metacoding.junitproject.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.util.MailSenderStub;
+import site.metacoding.junitproject.web.dto.BookResponseDto;
+import site.metacoding.junitproject.web.dto.BookSaveRequestDto;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 public class BookServiceTest {
+
+    @Autowired
+    private BookRepository repository;
+
+    // 문제점
+    // 서비스만 테스트하고 싶지만, Repository 레이어가 함께 테스트 된다.
+    // Repository 레이어는 이미 테스트가 종료됐는데 굳이 무겁게 같이 할 필요가 없다.
+
+    // 해결책
+    // 가짜 환경을 만들어서 가짜 Repository 를 만들어주자.
+    // 그럼 진짜 Repository 를 메모리에 로딩할 필요가 없다.
+    @Test
+    public void save() {
+        // given
+        String title = "title";
+        String author = "author";
+        BookSaveRequestDto dto = BookSaveRequestDto.builder()
+            .title(title)
+            .author(author)
+            .build();
+
+        // stub
+        MailSenderStub mailSenderStub = new MailSenderStub();
+
+        // when
+        BookService bookService = new BookService(repository, mailSenderStub);
+        BookResponseDto saveBookDto = bookService.save(dto);
+
+        // then (실제 값 / 예상 값)
+        assertThat(saveBookDto.getTitle()).isEqualTo(dto.getTitle());
+        assertThat(saveBookDto.getAuthor()).isEqualTo(dto.getAuthor());
+
+    }
 }
