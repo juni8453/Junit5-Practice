79659c7a79e2cf07934728aae55e5921bd8575df
ByeoungJoon Jeon <juni8453@naver.com>
2022-09-24 22:19:34 +0900

    :white_check_mark: BookApiController 단건 조회, 삭제, 수정 API 작성 완료
    #description
    1. 삭제 API 는 Body 가 없어서 204 No Content 가 맞지만, 현재 여기선 커스텀 응답 객체를 사용하기 때문에 Body 를 따로 응답해주고 있기 때문에 200 OK 를 응답하도록 했다.

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -39,6 +39,8 @@ public class BookApiController {
     }
 
     // 2. 책 목록 조회
+    // 클라이언트로 응답 시 컬렉션 자체로 넘겨주는 것은 파싱에 어려움을 야기할 수 있다.
+    // 최대한 Object 로 변경 후 줄 수 있도록 하자.
     @GetMapping("/book")
     public ResponseEntity<?> getBooks() {
         BookListResponseDto findBooks = bookService.getBooks();
@@ -52,20 +54,42 @@ public class BookApiController {
     }
 
     // 2-1. 책 단건 조회
-//    @GetMapping
-    public ResponseEntity<BookResponseDto> getBook() {
-        return null;
+    @GetMapping("/book/{id}")
+    public ResponseEntity<?> getBook(@PathVariable Long id) {
+        BookResponseDto findBook = bookService.getBook(id);
+        CMResponseDto<?> cmResponseDto = CMResponseDto.builder()
+            .code(1)
+            .message("책 단건조회 성공")
+            .body(findBook)
+            .build();
+
+        return new ResponseEntity<>(cmResponseDto, HttpStatus.OK);
     }
 
     // 3. 책 삭제
-//    @DeleteMapping
-    public void deleteBook() {
+    @DeleteMapping("/book/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.delete(id);
+        CMResponseDto<?> cmResponseDto = CMResponseDto.builder()
+            .code(1)
+            .message("책 삭제 성공")
+            .build();
 
+        // 204 No Content : 요청은 성공했지만 응답해줄 Body 가 빈 경우
+        // 하지만 우리는 CMResponseDto 란 커스텀 응답 객체를 사용하기 때문에 200 OK 를 사용한다.
+        return new ResponseEntity<>(cmResponseDto, HttpStatus.OK);
     }
 
     // 4. 책 수정
-//    @PostMapping
-    public void updateBook() {
+    @PostMapping("/book/{id}")
+    public ResponseEntity<?> updateBook(@PathVariable Long id, @Valid @RequestBody BookSaveRequestDto dto) {
+        BookResponseDto updateBook = bookService.update(id, dto);
+        CMResponseDto<?> cmResponseDto = CMResponseDto.builder()
+            .code(1)
+            .message("책 내용수정 성공")
+            .body(updateBook)
+            .build();
 
+        return new ResponseEntity<>(cmResponseDto, HttpStatus.OK);
     }
 }
