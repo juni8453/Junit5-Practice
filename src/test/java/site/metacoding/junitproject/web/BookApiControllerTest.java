eac924695049255c90c01535a92f280b22406306
ByeoungJoon Jeon <juni8453@naver.com>
2022-10-03 12:00:23 +0900

    :art: Book Controller 책 목록보기 테스트 작성완료
    #description
    - 클라이언트로부터 받아오는 데이터가 없기 때문에 given 공백 후 데이터 값을 미리 셋팅해주기 위해 @BeforeEach 어노테이션을 사용해 Book 객체를 미리 하나 셋팅
    - code, title, author 테스트 완료

--- src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
+++ src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
@@ -7,14 +7,16 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
 import org.springframework.http.*;
-import site.metacoding.junitproject.service.BookService;
+
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
 import site.metacoding.junitproject.web.dto.request.BookSaveRequestDto;
 
 import static org.assertj.core.api.Assertions.*;
@@ -26,6 +28,9 @@ public class BookApiControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     // @BeforeAll 을 통해 모든 테스트 메소드에서 사용되는 객체를 테스트 클래스 한 번에 초기화 하기 위해 static 선언
     // @BeforeAll 은 static 선언이 되어있는 객체만 정의할 수 있다.
@@ -39,6 +44,19 @@ public class BookApiControllerTest {
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
+    @BeforeEach
+    public void initData() {
+        String title = "title" ;
+        String author = "author" ;
+        Book book = Book.builder()
+            .title(title)
+            .author(author)
+            .build();
+
+        bookRepository.save(book);
+    }
+
+    // 1. 책 저장 테스트
     @Test
     public void saveTest() throws JsonProcessingException {
         // given
@@ -63,5 +81,26 @@ public class BookApiControllerTest {
 
         assertThat(bodyTitle).isEqualTo(dto.getTitle());
         assertThat(bodyAuthor).isEqualTo(dto.getAuthor());
+    }
+
+    // 2. 책 목록보기 테스트
+    @Test
+    public void readBooksTest() {
+        // given
+
+        // when
+        // GET 이기 떄문에 Body 없음 -> null
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange("/api/v1/book", HttpMethod.GET, request, String.class);
+
+        // then
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        int code = documentContext.read("$.code");
+        String title = documentContext.read("$.body.books[0].title");
+        String author = documentContext.read("$.body.books[0].author");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("title");
+        assertThat(author).isEqualTo("author");
     }
 }
